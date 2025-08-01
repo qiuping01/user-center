ad9746571e7aff7115e63d45360538f3d793db1a
qiuping01 <2187718660@qq.com>
2025-08-01 23:14:49 +0800

    导入junit单元测试库测试

--- src/test/java/com/ping/usercenter/SampleTest.java
+++ src/test/java/com/ping/usercenter/SampleTest.java
@@ -3,25 +3,29 @@ package com.ping.usercenter;
 import com.baomidou.mybatisplus.core.toolkit.Assert;
 import com.ping.usercenter.mapper.UserMapper;
 import com.ping.usercenter.model.User;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class SampleTest {
 
-    //
-    @Autowired
+    //@Resource默认会按照JavaBean的名称去注入
+    //@Autowired //只会按照类型去注入
+    @Resource
     private UserMapper userMapper;
 
     @Test
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        Assert.isTrue(5 == userList.size(), "");
+        Assert.isTrue(5 == userList.size(), ""); //断言：我觉得
         userList.forEach(System.out::println);
     }
-
 }
