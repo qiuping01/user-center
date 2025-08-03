c1c75d48d5e935fab0740b3153aa43cc78802b42
qiuping01 <2187718660@qq.com>
2025-08-03 11:09:42 +0800

    用户注册

--- src/main/java/com/ping/usercenter/service/UserService.java
+++ src/main/java/com/ping/usercenter/service/UserService.java
@@ -8,7 +8,37 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @description 针对表【user(用户)】的数据库操作Service
 * @createDate 2025-08-03 07:46:33
  * 业务接口，只定义方法不去实现
+ *
+ * 用户服务
+ *
+ * @author ping
 */
 public interface UserService extends IService<User> {
 
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    long userRegister(String userAccount, String userPassword,
+                      String checkPassword);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/ping/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/ping/usercenter/service/impl/UserServiceImpl.java
@@ -1,21 +1,85 @@
 package com.ping.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ping.usercenter.model.domain.User;
 import com.ping.usercenter.service.UserService;
 import com.ping.usercenter.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
-* @author 21877
-* @description 针对表【user(用户)】的数据库操作Service实现
-* @createDate 2025-08-03 07:46:33
+ * @author 21877
+ * @author ping
+ * @description 针对表【user(用户)】的数据库操作Service实现
+ * @createDate 2025-08-03 07:46:33
  * 实现类，实现接接口的业务
-*/
+ * <p>
+ * 用户服务实现类
+ */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements UserService{
+        implements UserService {
+    
+    /**
+     * 盐值，混淆密码
+     */
+    private static final String SALT = "ping";
 
+    /**
+     *
+     * 用户注册
+     *
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+        }
+        if (userAccount.length() < 4) {
+            return -1;
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            return -1;
+        }
+        // 账户不能包含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return -1;
+        }
+        // 密码和校验密码相同
+        if (checkPassword.equals(userPassword)) {
+            return -1;
+        }
+        // 账户不能重复,查询数据库尽量放到后面，以免浪费资源
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = this.count(queryWrapper);
+        if (count > 0) {
+            return -1;
+        }
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        // 3. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user); //保存到数据库中
+        if (!saveResult) {
+            return -1;
+        }
+        return user.getId();
+    }
 }
 
 
